47ae783a4a4b4f873da6ae07bbd825c6aad6cb07
Kevin Sandoval <ksandovaldev@gmail.com>
2021-02-27 22:43:44 -0500

    adding tic tac toe branch

---
+++ TTT/main.js
@@ -0,0 +1,11 @@
+document.querySelector('.newGame').addEventListener('click', button)
+
+
+
+let currentGame = null
+const board = new Board()
+
+function button(){
+  board.clear()
+  // currentGame = new Game()
+}
