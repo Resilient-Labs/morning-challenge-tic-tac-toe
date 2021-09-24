7372668e4c2d2beba8fc68a78ad5807a6b06bea5
Ian Ascencio <ian.ascencio.mancia@gmail.com>
2021-09-24 17:58:37 -0400

    first push

---
+++ js/oop.js
@@ -0,0 +1,48 @@
+class Game {
+  constructor() {
+    this.reset()
+  }
+
+  isValidMove(index) {
+    return this.board[index] === undefined && !this.isWon ? true : false
+  }
+
+  fillBox(index) {
+    if (this.isValidMove(index)) {
+      this.board[index] = this.player
+      this.player === 'x' ? this.player = 'o' : this.player = 'x'
+      this.emptyBoxes = this.emptyBoxes - 1
+      this.checkWin()
+    }
+  }
+
+  checkWin() {
+    for (let i = 0; i < this.winConditions.length; i++) {
+      const boxOne = this.board[this.winConditions[i][0]]
+      const boxTwo = this.board[this.winConditions[i][1]]
+      const boxThree = this.board[this.winConditions[i][2]]
+
+      if (boxOne && boxOne === boxTwo && boxTwo === boxThree) {
+        this.isWon = true
+        console.log(this.isWon)
+      }
+    }
+  }
+// I worked on this part creating the nested array with the win conditions.
+  reset() {
+    this.board = new Array(9) 
+    this.player = 'o'
+    this.isWon = false
+    this.emptyBoxes = 9
+    this.winConditions = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+  }
+}
