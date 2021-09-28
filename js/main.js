716308186e7843f52af06fe2215051894852fdcb
Karim Hassan <karimhassan92@gmail.com>
2021-09-27 21:19:08 -0400

    Fully OOP Tic-Tac-Toe

--- js/main.js
+++ js/main.js
@@ -1,144 +1,52 @@
-// create element for qSelector game status
-const displayStatus = document.querySelector('.gameStatus')
-const displayTurn = document.querySelector('.playerTurn')
-//function that returns objetcs
-const makePLayer = (name, score) => {
-  return {
-    name, 
-    score
-  };
-};
-// make player1 and make player2
-let player1 = makePLayer('Player 1', 'O')
-let player2 = makePLayer('Player 2', 'X' )
-//counter
-let counter = 0;
-//win/draw messages
-const winMessage1 = () => `${player1.name} is the winner!`
-const winMessage2 = () => `${player2.name} is the winner!`
-const drawMessage = () => `Game ended in a draw!`
-const restartMessage = () => `Please Click Restart Game`
-//event listerners
-document.getElementById('gameContainer').addEventListener("click", playerMove)
-document.querySelector('.gameRestart').addEventListener('click', handleRestartGame )
-//function adds inner text to target from players object.
-//counter++ for each iteration of loop
-//run checkwin() function to alter the DOM
-function playerMove(event) {
-  event.target = document.querySelectorAll('box');
-  if((event.target.innerText === player1.score ) || (event.target.innerText === player2.score)){
-    return 
-  } else{
-    if(counter % 2 === 0){
-      event.target.innerText = player1.score;
-      displayTurn.innerHTML = `Go: ${player2.name}`
+// Create an instance of the game
+const game = new TicTacToe();
+
+// Query Selectors
+const container = document.querySelector("#gameContainer");
+const restartButton = document.querySelector(".gameRestart");
+const boxes = document.querySelectorAll(".box");
+const gameStatus = document.querySelector(".gameStatus");
+const playerTurn = document.querySelector('.playerTurn')
+
+// Event Listeners Callback functions
+function fillBox(e) {
+  const index = [...boxes].indexOf(e.target);
+  if (e.target.className === "box" && game.isValidMove(index))  {
+    e.target.textContent = game.player;
+    game.playerMove(index);
+    // If the game is won, set the game status to whoever won
+    if (game.isWon){
+      gameStatus.innerText = game.player === "X" ? (game.player = "Player 1 has won") : (game.player = "Player 2 has won"); 
       
     }
-    else{
-      event.target.innerText = player2.score;
-      displayTurn.innerHTML = `Go: ${player1.name}`
+    if (!game.isWon && game.counter === 9){
+      gameStatus.innerText = `There has been a draw`
     }
-    counter++
+    if (game.counter % 2 === 0 || game.counter == 0){
+      playerTurn.innerText = `Go: Player 1`
+    }
+    else{
+      playerTurn.innerText = `Go: Player 2`
+    }
   }
-  checkWin();
-}
-//the bane of my existence
-function checkWin(){
-let arr = document.querySelectorAll('.box')
-if (displayStatus.innerText === winMessage1() || displayStatus.innerText === winMessage2() || displayStatus.innerText === drawMessage()){
-  displayTurn.innerText = restartMessage();
-  return;
-  //Shoutout David Ney for fixing a bug in my code with the logic above
-}
-if (arr[0].innerText === arr[1].innerText && arr[1].innerText === arr[2].innerText && arr[0].innerText == "O"){
-    displayStatus.innerText = winMessage1();
+    if (game.isWon || game.counter === 9){
+      playerTurn.innerText = `Please click restart game`
+    }
 }
 
-else if (arr[0].innerText === arr[1].innerText && arr[1].innerText === arr[2].innerText && arr[0].innerText == "X"){
-    displayStatus.innerText = winMessage2();
-}
-else if (arr[3].innerText === arr[4].innerText && arr[4].innerText === arr[5].innerText && arr[3].innerText == "O"){
-  displayStatus.innerText = winMessage1();
-}
+function restartGame() {
+  for (box of boxes ){
+    box.innerText = ''
+    gameStatus.innerText = ''
+    game.counter = 0;
+    playerTurn.innerText = 'Go: Player 1'
 
-else if (arr[3].innerText === arr[4].innerText && arr[4].innerText === arr[5].innerText && arr[3].innerText == "X"){
-  displayStatus.innerText = winMessage2();
-}
-
-else if (arr[6].innerText === arr[7].innerText && arr[7].innerText === arr[8].innerText && arr[6].innerText == "O"){
-  displayStatus.innerText = winMessage1();
-}
-
-else if (arr[6].innerText === arr[7].innerText && arr[7].innerText === arr[8].innerText && arr[6].innerText == "X"){
-displayStatus.innerText = winMessage2();
-}
-
-else if (arr[0].innerText === arr[3].innerText && arr[3].innerText === arr[6].innerText && arr[0].innerText == "O"){
-  displayStatus.innerText = winMessage1();
-}
-
-else if (arr[0].innerText === arr[3].innerText && arr[3].innerText === arr[6].innerText && arr[0].innerText == "X"){
-  displayStatus.innerText = winMessage2();
-}
-
-else if (arr[1].innerText === arr[4].innerText && arr[4].innerText === arr[7].innerText && arr[1].innerText == "O"){
-  displayStatus.innerText = winMessage1();
-}
-
-else if (arr[1].innerText === arr[4].innerText && arr[4].innerText === arr[7].innerText && arr[1].innerText == "X"){
-  displayStatus.innerText = winMessage2();
-}
-
-else if (arr[2].innerText === arr[5].innerText && arr[5].innerText === arr[8].innerText && arr[2].innerText == "O"){
-  displayStatus.innerText = winMessage1();
-}
-
-else if (arr[2].innerText === arr[5].innerText && arr[5].innerText === arr[8].innerText && arr[2].innerText == "X"){
-  displayStatus.innerText = winMessage2();
-}
-
-else if (arr[0].innerText === arr[4].innerText && arr[4].innerText === arr[8].innerText && arr[0].innerText == "O"){
-  displayStatus.innerText = winMessage1();
-}
-
-else if (arr[0].innerText === arr[4].innerText && arr[4].innerText === arr[8].innerText && arr[0].innerText == "X"){
-  displayStatus.innerText = winMessage2();
-}
-
-else if (arr[2].innerText === arr[4].innerText && arr[4].innerText === arr[6].innerText && arr[2].innerText == "O"){
-  displayStatus.innerText = winMessage1();
-}
-
-else if (arr[2].innerText === arr[4].innerText && arr[4].innerText === arr[6].innerText && arr[2].innerText == "X"){
-  displayStatus.innerText = winMessage2();
-}
-
-else if (counter === 9){
-  displayStatus.innerText = drawMessage();
-}
-updateGame();
-}
-//Words can't describe how sorry I am for this logic
-
-//function for restart button that removes inner text for all box class objects and restarts counter
-function handleRestartGame() {
-  document.querySelectorAll('.box').forEach(cell => cell.innerText = "");
-  counter = 0;
-  displayStatus.innerText = "";
-  displayTurn.innerText = `Go: ${player1.name}`;
-}
-//function for changing DOM if the game result has been decided
-function updateGame(){
-  if(displayStatus.innerText == winMessage1() || displayStatus.innerText == winMessage2() || displayStatus.innerText == drawMessage()){
-    displayTurn.innerText = restartMessage();
+    
   }
+  game.restartGame()
+  console.log(restartGame)
 }
-        
 
-
-
-
-
-
-
-
+// Attach Event Handlers
+container.addEventListener("click", fillBox);
+restartButton.addEventListener("click", restartGame);
--- js/tictactoe.js
+++ js/tictactoe.js
@@ -1,79 +1,60 @@
-
 class TicTacToe {
-	constructor() {
-		this.board = new Array(9)
-		this.player = 'O'
-		this.isWon = false
-    this.displayStatus = document.querySelector('.gameStatus')
-    this.displayTurn = document.querySelector('.playerTurn')
-    
-		this.winConditions = [
-			[0, 1, 2],
-			[3, 4, 5],
-			[6, 7, 8],
-			[0, 3, 6],
-			[1, 4, 7],
-			[2, 5, 8],
-			[0, 4, 8],
-			[2, 4, 6],
-		]; 
-	}
+  constructor() {
+    this.restartGame();
+		this.counter = 0;
+  }
 
-	checkWin() {
-		for (let condition of this.winConditions) {
-      const box1 = this.board[condition[0]]
-      const box2 = this.board[condition[1]]
-      const box3 = this.board[condition[2]]
-      if (box1 && box1 === box2 && box2 === box3){
-        this.displayStatus.innerText = 'Player won'
-        this.displayTurn.innerText = 'Please click restart game'
+  checkWin() {
+    for (let condition of this.winConditions) {
+      const box1 = this.board[condition[0]];
+      const box2 = this.board[condition[1]];
+      const box3 = this.board[condition[2]];
+      if (box1 && box1 === box2 && box2 === box3) {
         this.isWon = true;
-        
-				
       }
-		}
-	}
+    }
+  }
 
-	isValidMove(index) {
-		return this.board[index] === undefined && !this.isWon ? true : false
-		// return this.board[index] ? false : true
-	}
+  isValidMove(index) {
+	// Don't let the user fill a box if:
+	// 		1. it's already taken
+	// 		2. the game is already won
+    return this.board[index] || this.isWon ? false : true;
+  }
 
-	playerMove(index) {
-   
-		if (!this.isValidMove(index)){
-		this.board[index] = this.player
-		this.player === 'X' ? this.player = 'O' : this.player = 'X'
-		this.emptyBoxes = this.emptyBoxes - 1
-		this.checkWin()
-		}
-	}
+  playerMove(index) {
+    // Check if the move is valid (is the box taken or is the game already won)
+    if (this.isValidMove(index)) {
+      // fill in the board with the user's move
+      this.board[index] = this.player;
+      // Switch/Change over to the next person
+      this.player === "X" ? (this.player = "O") : (this.player = "X");
+      // Check if there's a win at the end of every turn
+			this.counter++
+      this.checkWin();
+			
 
-  restartGame(){
-  document.querySelector('.gameRestart').onclick = function x (){
-  document.querySelectorAll('.box').forEach(box => box.innerText = "")
-}}
+    }
+		console.log(this.counter)
+  }
 
-printToDom (e){
-  let index = Array.from(e.target.parentElement.children).indexOf(e.target)
-  console.log(index)
-  e.target.innerText = newTicTacToe.playerMove(index)
-  newTicTacToe.checkWin();
-  newTicTacToe.restartGame();
+  restartGame() {
+    this.board = new Array(9);
+    this.player = "O";
+    this.isWon = false;
+    this.winConditions = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+  }
 }
 
-
-}
-
-document.getElementById('gameContainer').addEventListener("click", printToDom)
-let newTicTacToe = new TicTacToe()
-function printToDom (e){
-  let index = Array.from(e.target.parentElement.children).indexOf(e.target)
-  console.log(index)
-  e.target.innerText = newTicTacToe.playerMove(index)
-  newTicTacToe.checkWin();
-  newTicTacToe.restartGame();
-}
 
 
 
