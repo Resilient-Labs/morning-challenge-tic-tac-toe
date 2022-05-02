e1d0c6e491e93a294dc0b7e7993c2ed78dc69a4d
Travora Senecharles <travora9@gmail.com>
2022-05-02 05:38:51 -0400

    This broke my brain and I loved it. I have a lot more practice to do, but that's okay.

--- README.md
+++ README.md
@@ -2,21 +2,9 @@
 
 ### Goal: Create a two player Tic-Tac-Toe game. The users should be able to click to place their X or O and if they win the program should mention their win in the DOM. Please make the game as OOP as possible.
 
-### How to submit your code for review:
-
-- Fork and clone this repo
-- Create a new branch called answer
-- Checkout answer branch
-- Push to your fork
-- Issue a pull request
-- Your pull request description should contain the following:
-  - (1 to 5 no 3) I completed the challenge
-  - (1 to 5 no 3) I feel good about my code
-  - Anything specific on which you want feedback!
-
+I completed the challenge: 4
+I feel good about my code: 5
+I know that my code isn't perfect and I have questions about a couple things. Despite that, I worked really hard to understand OOP and worked with a lot of people and a few mentors in order to really comprehend how to take code that is mostly function based and make it OOP. I put a 4 for challenge completion because I haven't gotten my win message to specify which player won. I got stuck in a recursive loop according to google (I got htis error in my console and googled it: Uncaught RangeError: Maximum call stack size exceeded) and I'm not really sure how. Google did mention repetitive code and mine was certainly repetitive in that section, but I'm not sure how to fix it and it's 5:35 AM so I'm gonna fix it tomorrow with an EIR.
 Example:
 ```
-I completed the challenge: 5
-I feel good about my code: 4
-I'm not sure if my constructors are setup cleanly...
-```
+
--- main.js
+++ main.js
@@ -1,88 +1,102 @@
-
 class MakePlayer{
   constructor(exOrOh, playerTurn){
-  this.selections = []                         
-  this.exOrOh = exOrOh                        
-  this.playerTurn = playerTurn
-} 
+    this.selections = []                         
+    this.exOrOh = exOrOh                        
+    this.playerTurn = playerTurn
+  } 
   
   makeMove(boxIndex) {              
-  this.selections.push(boxIndex)   
-}
+    this.selections.push(boxIndex)   
+  }
 }
 
-let playerX = new MakePlayer ('X', true)        
-let playerO = new MakePlayer ('O', false)       
-let winnerMessage = document.querySelector(".whoWon")
-let turnCount = 0
-let winStatus = false
+class Game {
+  constructor() {
+    this.playerX = new MakePlayer ('X', true)     //this playerX is actually an instance of the MakePlayer 
+    this.playerO = new MakePlayer ('O', false)
+    this.winStatus = false
+    this.turnCount = 0
+    this.winnerMessage = document.querySelector(".whoWon") 
+    this.currentTurnMessage = document.querySelector('.playerTurn')
+  }
+  whenBoxClicked(e){  
+    if((e.target.innerText === "") && (game.turnCount < 9)){ 
+      game.turnCount++
+      console.log(game.turnCount)
+      e.target.innerText = currentPlayer.exOrOh
+      game.changePlayer() 
+      }  
+    let idValue = Number(e.target.getAttribute('id'));         
+    currentPlayer.makeMove(idValue) 
+      game.checkWin()                    
+      if(game.checkWin() === true){
+        console.log('You won')
+        game.winnerMessage.innerText = 'You won' 
+      }               
+  } 
+  changePlayer (){
+    console.log("changePlayer")
+    if(currentPlayer === game.playerX){  
+        currentPlayer = game.playerO
+        game.currentTurnMessage.innerText = "Player O's turn"
+      }else{
+        currentPlayer = game.playerX    
+        game.currentTurnMessage.innerText = "Player X's turn" 
+      }  
+  }
+  checkWin(){
+    winningConditions.forEach(condition => { 
+      let matchList = condition.filter(boxNumber =>   currentPlayer.selections.includes(boxNumber)) 
+        if(matchList.length === 3){   
+          game.winStatus = true            
+          return true                
+        }
+      })
+    if(game.turnCount === 9 && game.winStatus === false){
+      console.log('draw')
+      console.log(game.winStatus)
+      console.log(game.turnCount)
+      game.winnerMessage.innerText='draw'
+      
+    }
+    return game.winStatus                 
+  }
+  resetGame() {
+    game.turnCount = 0
+    game.winStatus = false
+    let eachBox = document.querySelectorAll('.box')
+    eachBox.forEach(box => {
+      box.innerText = ''
+    })
+    game.winnerMessage.innerText = ''
+    game.currentTurnMessage.innerText = "Player X's turn"
+    game.playerX.selections = []  
+    game.playerO.selections = []
+  }
 
-let currentPlayer = playerX                               
+}
+
+let game = new Game
+let currentPlayer = game.playerX  
 
 const boxValue = document.querySelector(".boxes")   
-boxValue.addEventListener('click', whenBoxClicked)  
+boxValue.addEventListener('click', game.whenBoxClicked)  
 
 let clear = document.querySelector("#clear")
-clear.addEventListener('click', resetGame)
-
-  
-
-function whenBoxClicked(e){  
-  if((e.target.innerText === "") && (turnCount < 9)){ 
-    turnCount++
-    console.log(turnCount)
-    e.target.innerText = currentPlayer.exOrOh
-    changePlayer() 
-    }  
-  let idValue = Number(e.target.getAttribute('id'));           
-  currentPlayer.makeMove(idValue) 
-    checkWin()                    
-    if(checkWin() === true ){
-      console.log('You won')
-      winnerMessage.innerText = 'You won' 
-    }               
-
-}
-
-function changePlayer (){
-  console.log("changePlayer")
-  if(currentPlayer === playerX){  
-      currentPlayer = playerO
-    }else{
-      currentPlayer = playerX     
-    }  
-}
-
-function checkWin(){
-  winningConditions.forEach(condition => { 
-    let matchList = condition.filter(boxNumber =>   currentPlayer.selections.includes(boxNumber)) 
-      if(matchList.length === 3){   
-        winStatus = true            
-        return true                
-      }
-    })
-  if(turnCount === 9 && winStatus === false){
-    console.log('draw')
-    console.log(winStatus)
-    console.log(turnCount)
-    winnerMessage.innerText='draw'
-  }
-  return winStatus                 
-} 
-
-function resetGame() {
-  turnCount = 0
-  winStatus = false
-  let eachBox = document.querySelectorAll('.box')
-  eachBox.forEach(box => {
-    box.innerText = ''
-  })
-
-  winnerMessage.innerText = ''
-  playerX.selections = []  
-  playerO.selections = []
-}
+clear.addEventListener('click', game.resetGame)
 
 let winningConditions = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]] 
+
+//keep moving code into game class
+//id state of game - what aspects of game are changing over time
+//define instance properties to reference those
+//update code to reference correctly
+// //turncount variable --
+//winStatus  -- 
+//idValue dub 
+//winnerText --
+//matchList dub
+
+
 
 
