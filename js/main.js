4bc556f3e99fa8eea64b3e44e3ae7548e4597080
Emily Castillo <emilycastillox@gmail.com>
2022-05-07 22:25:30 -0400

    fixed reset

--- js/main.js
+++ js/main.js
@@ -89,14 +89,7 @@ function checkWin(){
 
 
 function resetGame() {
-    turnCount = 0
-    winStatus = false
-    winMsg.innerText = ''
-    whichPlayer.innerHTML = ''
-    let eachBox = document.querySelectorAll(".box")
-    eachBox.forEach(box => {
-    box.innerText = ''
-        })
+    location.reload()
  }
 
 let winningConditions = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]] //all possible winning combinations
