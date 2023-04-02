958d7c163bd33e08891bceebcebf7aed3e45c1ac
bakitucker <bakitucker8@gmail.com>
2023-04-02 16:53:20 -0400

    update but not finished yet'

--- js/main.js
+++ js/main.js
@@ -2,31 +2,26 @@
 // box will say x or o
 // is there a win
 
+
+// mike told me to fix this bug(player is allowed to click on a box thats already been selected)
 let player = "X"
-
-
-
-
-
 class Box {
     constructor(boxNum){
         this.boxNum = boxNum
         this.owner = ''
+        
     }
 
-
         PickOne(){
-        console.log(player)
         document.querySelectorAll('.box')[this.boxNum].src = player+'.png'
         this.owner = player
         CheckIfWin()
         togglePlayer()
-    }
-    
-
-
+    } 
 
 }
+
+
 
 function CheckIfWin (){
     // winner winning first row
@@ -34,17 +29,25 @@ function CheckIfWin (){
         // alert (`Player ${player} has won!!!`)
         document.querySelector('h1').innerText = `Player ${player} has won!!!`
     } else if(arr[0].owner === arr[4].owner && arr[0].owner === arr[8].owner && arr[4].owner === player){
-        alert (`Player ${player} has won!!!`)
-    }
-    
-    
-    else if (arr[0].owner.length + arr[1].owner.length + arr[2].owner.length +
+        document.querySelector('h1').innerText = `Player ${player} has won!!!`
+    } else if(arr[3].owner === arr[4].owner && arr[3].owner === arr[5].owner && arr[4].owner === player){
+        document.querySelector('h1').innerText = `Player ${player} has won!!!`
+    } else if(arr[6].owner === arr[7].owner && arr[6].owner === arr[8].owner && arr[6].owner === player){
+        document.querySelector('h1').innerText = `Player ${player} has won!!!`
+    } else if(arr[0].owner === arr[3].owner && arr[0].owner === arr[6].owner && arr[0].owner === player){
+        document.querySelector('h1').innerText = `Player ${player} has won!!!`
+    } else if(arr[1].owner === arr[4].owner && arr[1].owner === arr[7].owner && arr[4].owner === player){
+        document.querySelector('h1').innerText = `Player ${player} has won!!!`
+    } else if(arr[2].owner === arr[5].owner && arr[2].owner === arr[8].owner && arr[2].owner === player){
+        document.querySelector('h1').innerText = `Player ${player} has won!!!`
+    } else if(arr[2].owner === arr[4].owner && arr[2].owner === arr[6].owner && arr[2].owner === player){
+        document.querySelector('h1').innerText = `Player ${player} has won!!!`
+    } else if (arr[0].owner.length + arr[1].owner.length + arr[2].owner.length +
         arr[3].owner.length + arr[4].owner.length + arr[5].owner.length +
         arr[6].owner.length + arr[7].owner.length + arr[8].owner.length === 9){
             alert ('tie')
         }
 }
-
 
 function togglePlayer(){
    if( player === "O" ){
@@ -58,6 +61,7 @@ let arr = []
 for(let i = 0; i<=8; i++){
     let boxes = new Box(i)
     arr.push(boxes)
+    console.log(boxes)
     document.querySelectorAll('.box')[i].addEventListener('click', function(){
     boxes.PickOne()
     })
@@ -68,41 +72,3 @@ for(let i = 0; i<=8; i++){
 
 
 
-// var boesx = new Box(i);
-// document.getElementById('x').addEventListener('click', function () {
-//   object.method()
-// }
-
-
-// const baki = {
-//     Name : 'bakiTucker' ,
-//     favoriteColor: ['Blue','Red','Purple'],
-//     age: 26
-// }
-// const foo = baki
-
-
-// baki.Name = 'mike'
-
-// console.log(baki.Name)
-// console.log(foo.Name)
-
-
-// class Car {
-//     // blueprint
-//     constructor(name, make, model, year){
-//         this.name = name
-//         this.make = make
-//         this.model = model
-//         this.year = year
-//     }
-
-//     drive(){
-//         console.log(`my muthafuckin ${this.name}`)
-//     }
-// }
-
-// const mazda = new Car('Mazda', 'cx3', 'crashed', 2005)
-    
-// console.log(mazda)
-// console.log(mazda.drive())
