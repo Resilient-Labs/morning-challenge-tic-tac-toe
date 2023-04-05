e6fbfc5223a0f933f5405e69bf91def636e69365
wadiya korpoi <wadiyakorpoi@gmail.com>
2023-04-05 07:30:14 -0400

    updated

--- script.js
+++ script.js
@@ -1,33 +1,54 @@
+// Variables
 const gameBoard = document.querySelector("#gameboard")
 const infoDisplay = document.querySelector("#info")
+
+// Create Board
 const startCells = [ 
     "","","","","","","","","",
 ]
-let go = "circle" 
-infoDisplay.textContent = "Circle goes first"
 
+// Who starts
+let go = "white" 
+
+// Display of who goes first. 
+infoDisplay.textContent = "White goes first"
+
+// Create function that creates the board.
 function createBoard() {
+
+    // grab the squaes array for each element gab the quare and th eindex and create an element.
     startCells.forEach((cell, index) => {
        const cellElement = document.createElement("div")
+    //    add class for each square
        cellElement.classList.add("square")
+    //    assign an ID to the square, numbering them individially. 
        cellElement.id = index
+    // listen for the click .
        cellElement.addEventListener('click', addGo)
+    //    put the squares on the board.
        gameBoard.append(cellElement)
 })
 }
 
 createBoard()
 
-function addGo(e){
+// this the function that adds the circles.
+function addGo(e){ 
+    // creates the circle element 
     const goDisplay = document.createElement('div')
+    // create a class for the div to display a cicrle. 
     goDisplay.classList.add(go)
+    // ttargets the event & adds the circle. 
     e.target.append(goDisplay)
-    go = go === "circle" ? "cross" : "circle"
-    infoDisplay.textContent = "it is now " + go + " 's turn."
+    // if go = the string white then change to pink, otherswise white.
+    go = go === "white" ? "pink" : "white" 
+    infoDisplay.textContent = `it is now ${go}'s turn.`
+    // removes event listener after the one click, so that the same box cannot be chosen again.
     e.target.removeEventListener("click", addGo)
     checkScore()
 }
 
+// function to check the score. 
 function checkScore(){
     
     const winningCombos = [
@@ -35,27 +56,27 @@ function checkScore(){
         [0,3,6], [1,4,7], [2,5,8],
         [0,4,8], [2,4,6]
     ]
-let allSquares = document.querySelectorAll(".square")
+    // checking all squares for winningCombos
+const allSquares = document.querySelectorAll(".square")
 
       winningCombos.forEach(array => {
-           const circleWins = array.every(cell => 
-                  allSquares[cell].firstChild?.classList.contains('circle'))
-     
-
-      if (circleWins) {
-            infoDisplay.textContent = "Circle Wins!"
+           const whiteWins = array.every(cell => 
+                  allSquares[cell].firstChild?.classList.contains('white'))
+    // condition if white wins
+      if (whiteWins) {
+            infoDisplay.textContent = "WHITE WINS!"
             allSquares.forEach(square => square.removeWith(square.cloneNode(true)))
             return 
       }
 })
-
+// doing the same for pink
 winningCombos.forEach(array => {
-      const crossWins = array.every(cell => 
-             allSquares[cell].firstChild?.classList.contains('cross'))
+      const pinksWins = array.every(cell => 
+             allSquares[cell].firstChild?.classList.contains('pink'))
 
 
- if (crossWins) {
-       infoDisplay.textContent = "Cross Wins!"
+ if (pinkWins) {
+       infoDisplay.textContent = "PINK WINS!"
        allSquares.forEach(square => square.removeWith(square.cloneNode(true)))
        return 
  }
