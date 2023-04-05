1778deaa25b7e04339b0a365b3d0a71b948a36ee
zyonnegb <zyonnegoodenbyrd@gmail.com>
2023-04-05 10:32:40 -0400

    edited javascript

--- main.js
+++ main.js
@@ -1,20 +1,22 @@
 let player = "X";
-const x = document.querySelector(".closeOne");
-const y = document.querySelector(".closeTwo");
+let x = document.querySelector(".closeOne");
+let y = document.querySelector(".closeTwo");
 let winner = document.getElementById("winner");
-let cells = document.querySelectorAll("#cells");
-cells = Array.from(cells); // this (Array.from()) turns html elements or node lists into an array.
+let cell = document.querySelectorAll("#cell");
 
-cells.forEach((cell) => {
-  // we are looping through each value in the array cells, passing it through the conditional below
+
+
+
+cell = Array.from(cell); // turns html elements into array
+cell.forEach((cell) => {  // looping through each value in the array, passing it through conditional
+
   cell.addEventListener("click", () => {
-    if (cell.innerText != "") {
-      // if cell is not an empty string aka if cell has an X or O in it, return aka exit
+    if (cell.innerText != "") {    // if cell has an X or O in it, return
       return;
     }
     cell.innerText = player; // if the string is empty, make the inner text player, which is equal to playerX.icon or playerO.icon
-    // function to check for winner
-    game.checkWinner();
+
+    game.checkWinner();    // check win
     player = player == playerX.icon ? playerO.icon : playerX.icon; // if player is equal to X , make player1 equal to O, else make player1 equal to X
   });
 });
@@ -27,56 +29,56 @@ class Player {
 
 class Game {
   checkWinner = () => {
-    let draw = cells.every(
+    let draw = cell.every(
       (element, index) =>
-        cells[index].innerText == "X" || cells[index].innerText == "O"
+        cell[index].innerText == "X" || cell[index].innerText == "O"
     );
     if (
-      cells[0].innerText == player &&
-      cells[1].innerText == player &&
-      cells[2].innerText == player
+      cell[0].innerText == player &&
+      cell[1].innerText == player &&
+      cell[2].innerText == player
     ) {
       winModal();
     } else if (
-      cells[0].innerText == player &&
-      cells[3].innerText == player &&
-      cells[6].innerText == player
+      cell[0].innerText == player &&
+      cell[3].innerText == player &&
+      cell[6].innerText == player
     ) {
       winModal();
     } else if (
-      cells[0].innerText == player &&
-      cells[4].innerText == player &&
-      cells[8].innerText == player
+      cell[0].innerText == player &&
+      cell[4].innerText == player &&
+      cell[8].innerText == player
     ) {
       winModal();
     } else if (
-      cells[1].innerText == player &&
-      cells[4].innerText == player &&
-      cells[7].innerText == player
+      cell[1].innerText == player &&
+      cell[4].innerText == player &&
+      cell[7].innerText == player
     ) {
       winModal();
     } else if (
-      cells[2].innerText == player &&
-      cells[4].innerText == player &&
-      cells[6].innerText == player
+      cell[2].innerText == player &&
+      cell[4].innerText == player &&
+      cell[6].innerText == player
     ) {
       winModal();
     } else if (
-      cells[2].innerText == player &&
-      cells[5].innerText == player &&
-      cells[8].innerText == player
+      cell[2].innerText == player &&
+      cell[5].innerText == player &&
+      cell[8].innerText == player
     ) {
       winModal();
     } else if (
-      cells[3].innerText == player &&
-      cells[4].innerText == player &&
-      cells[5].innerText == player
+      cell[3].innerText == player &&
+      cell[4].innerText == player &&
+      cell[5].innerText == player
     ) {
       winModal();
     } else if (
-      cells[6].innerText == player &&
-      cells[7].innerText == player &&
-      cells[8].innerText == player
+      cell[6].innerText == player &&
+      cell[7].innerText == player &&
+      cell[8].innerText == player
     ) {
       winModal();
     } else if (draw) {
@@ -84,24 +86,31 @@ class Game {
     }
   };
 }
-// MODALS
+
+const game = new Game();
+const playerX = new Player("X");
+const playerO = new Player("O");
+
+// MODAL AKA POP UP BOX 
+
 const winModal = () => {
-  document.getElementById("winModal").style.display = "block";
+  document.getElementById("winModal").style.display = "block"; // displays win box 
   winner.innerText = `${player} Wins!`;
 };
 const drawModal = () => {
-  document.getElementById("drawModal").style.display = "block";
+  document.getElementById("drawModal").style.display = "block"; // displays draw box
 };
+
+
+
+
+x.addEventListener("click", closeOne);
+y.addEventListener("click", closeTwo);
+
+
 const closeOne = () => {
   window.location.reload();
 };
 const closeTwo = () => {
   window.location.reload();
-};
-// EVENT LISTENERS
-x.addEventListener("click", closeOne);
-y.addEventListener("click", closeTwo);
-// INSTANTIATIONS
-const game = new Game();
-const playerX = new Player("X");
-const playerO = new Player("O");
+}
