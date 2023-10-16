9f4bedfa5868537e30f5018e60a0f46350a60e50
socratic617 <erik.solis02135@gmail.com>
2023-10-15 20:33:34 -0400

    updated pseudo code

--- js/main.js
+++ js/main.js
@@ -7,6 +7,10 @@
 //if my method takes in 3 parameters, how many arguements does it take?: three
 //can i put my parameters placeholder as my arguement?: NO
 //Why does this.board[null] not work?: because you need to put in a index and null isnt index
+//global variable to reference where I want is so i can refeernce it in multiple functions bc if i did it inside it other functions it would be a local scope not being able to acess
+//pass event as parameter to give me the event that happens
+// create a tenrary operator and create oconditon you are checking to see if first opotion is true , if not true then defualt to to false option which is player 1 in this case
+/* you are able to pass a string integer as an index to an array*/
 
 /******************************** (END) CODING NOTES **************************/
 
@@ -62,7 +66,7 @@
 /******************************** (END)TESTING MAP OBJECT **************************/
 
 /*------------------------------------------------------------------------------------
- *
+ *The player object represents the players of the game in tic tac toe
  *
  * --------------------------------------------------------------------------------- */
 class Player {
@@ -71,15 +75,18 @@ class Player {
     this.taunt = taunt;
     this.visualTaunt = visualTaunt;
   }
-  //GETTER METHODS
+
+  //gives what the player symbol is
   designatedSymbol() {
     return this.symbol;
   }
 
+  //gives what the player will say/taunt when they win
   sayTaunt() {
     return this.taunt;
   }
 
+  //gives what the player will show when they win with an img
   displayVisualTaunt() {
     return this.visualTaunt;
   }
@@ -99,36 +106,40 @@ class Map {
    * @param target object:  is the DOM element user selected to update
    *  corresponding to cell 2 or "0" corresponding to cell 1
    * @param  symbol string: the symbol of the current user (each player has either an X or an O)
-   * @return true or false  Boolean 
+   * @return true or false  Boolean returns if the user was able that selected cell
    * */
   updateCells(target, symbol) {
-    /* you are able to pass a string integer as an index to an array*/
+    
     if (this.board[target.id] === null) {
+
       this.board[target.id] = symbol;
       target.innerText = symbol;
       return true;
-      //To Do: add code to HTML for symbol to update
     }
     return false;
-    /* checking to see if array element of index is updated w/ symbol*/
-    console.log(this.board);
+
   }
 
   /* This method is used to reset the board and it clears the board tic tac toe
    * with all the users symbols of x's and o's and sets each cell index to null to start with no symbols
    * */
   clearCells() {
-    let cellItems =  document.querySelectorAll('div.cell-item')
+    //created a new variable to contain an array of the cell-items from their respective DOM element
+    let cellItems = document.querySelectorAll("div.cell-item");
+
+    //using the for each loop, i am looping through each cell item and removing the existing text
     cellItems.forEach((cellItem) => {
-      cellItem.innerText = ""
+      cellItem.innerText = "";
     });
-    
-    document.querySelector("#player-text").innerText = "";  
-    if (document.querySelector("#visual-reptar")){
-      document.querySelector("#visual-reptar").remove()
+
+    //removing the existing text 
+    document.querySelector("#player-text").innerText = "";
+
+    // using this conditional to handle two edge cases when either the game starts and has no visuals or when players tie , this is bc in both situations there would be no img element 
+    if (document.querySelector("#visual-reptar")) {
+      document.querySelector("#visual-reptar").remove();
     }
 
-    // document.querySelector("#visual-reptar").remove();
     /* check if board was cleared*/
     console.log(this.board);
   }
@@ -192,7 +203,7 @@ class Map {
     }
     return false;
   }
-  // check first row three in a row
+
 }
 
 /*------------------------------------------------------------------------------------
@@ -202,10 +213,8 @@ class Map {
 class Game {
 
   constructor() {
-    //clear x's and os if there was a previoud game played
 
-    // you need to create 2 game players
-    //Designate x and o to a player
+    // you need to create 2 game players. Designate x and o to a player
     this.player_1 = new Player(
       "🦖",
       "Hold on to your diapies, babies, I'm the WINNER",
@@ -217,74 +226,76 @@ class Game {
       "Hold on to your diapies, babies, I'm the WINNER",
       "img/chuckie-win.gif"
     );
-    //Designate which player starts and whos turn it is
+
+    //Designate which player starts and use to track whose turn it is
     this.turn = this.player_1.designatedSymbol();
-    // you need to create a map instance
+
+    // you need to a new board
     this.board = new Map();
-    this.board.clearCells()
 
+    //clear x's and os if there was a previoud game played
+    this.board.clearCells();
 
-    //create a property with the starting # of turns
+    //create a property with the starting # of turns to keep track of how many total turns are left
     this.totalTurns = 9;
-    console.log(
-      "*************************************************************"
-    );
-    console.log(this.totalTurns);
-    console.log(this.turn);
-    console.log(this.player_1);
-    console.log(this.player_2);
-    console.log(this.board);
-    console.log(
-      "*************************************************************"
-    );
+
+    //to confirm if the game ended or still live
     this.gameActive = true;
   }
 
   /* This method will allow current player to make their move to place their symbol
-   * then update whos turn it is to let the next player go by updating the 'turn' (property)
-   * @param target DOM element that user targeted by selecting. is needed to update and get id
+   * then update whos turn it is to let the next player go by updating the 'turn' (property).
+   * After every turn, check if the user won or tied and  if so, notify the user in the html.
+   * If the game is done do not allow the user to make any more moves.
+   * 
+   * @param target DOM element that user targeted by selecting. is needed to update and to get id
    * */
   takeTurn(target) {
-    //check to see if game is active
-    // if its not then it shouldnt be able to add anything
-    if(!this.gameActive){
+    //check to see if game is active. if its not then it shouldnt be able to do anything
+    if (!this.gameActive) {
       return;
     }
 
-
-    /** passing these twp parameters bc need id to identify where i will update and based on whos turn it is, ill know what symbol they use*/
+    /** passing these twp parameters bc I need id to identify what element I will updated and based on whos turn it is, that's whose symbol I will use*/
     let result = this.board.updateCells(target, this.turn);
-    // create a tenrary operator and create oconditon you are checking to see if first opotion is true , if not true then defualt to to false option which is player 1 in this case
-   
-    if(result){
-      this.totalTurns --
-      console.log("totalTurns: " + this.totalTurns)
-// if this players turn went successfully we want to confirm its there turn if they didnt win 
-// notify that they won and prevent the loser from clicking anything else
+
+  
+    /* Checking to see if current player made a valid move(didnt select an alreaady filled out
+      cell.) or it checks to see if you have no more turns*/
+    if (result) {
+      // If it was a valid move, update how many total moves we have left in the game
+      this.totalTurns--;
+      console.log("totalTurns: " + this.totalTurns);
+
+      //chekcks to see that there was a winner
       if (this.board.checkToSeeWinner(this.turn)) {
 
-        let currentPlayer = this.player_1.designatedSymbol() === this.turn ? this.player_1 : this.player_2;     
-        
-        document.querySelector('h3').innerText = currentPlayer.sayTaunt();
+        //this checks which player won
+        let currentPlayer =
+          this.player_1.designatedSymbol() === this.turn
+            ? this.player_1
+            : this.player_2;
 
-        let visual = document.createElement("img"); 
-        visual.id = "visual-reptar"
-        visual.src = currentPlayer.displayVisualTaunt(); 
-        document.querySelector("#visual-taunt").appendChild(visual);    
+        // this displays the text taught in the dom when the player wins
+        document.querySelector("h3").innerText = currentPlayer.sayTaunt();
 
-        // document.querySelector("#visual-taunt")
+        //this creates an img element, adds an id to it, then adds it to the div to display it
+        let visual = document.createElement("img");
+        visual.id = "visual-reptar";
+        visual.src = currentPlayer.displayVisualTaunt();
+        document.querySelector("#visual-taunt").appendChild(visual);
 
-
+        //this tells you the game ended and there is not any more valid moves to make in the game
         this.gameActive = false;
-        
-      }
-      else if(!this.totalTurns){
-        document.querySelector("h3").innerText =
-          "hey kiddos it's a tie";  
-          this.gameActive = false;
-      }
-      
+      } else if (!this.totalTurns) {
+        //displays it was a tie
+        document.querySelector("h3").innerText = "hey kiddos it's a tie";
 
+        //this tells you the game ended and there is not any more valid moves to make in the game
+        this.gameActive = false;
+      }
+
+      //this updates who the current player is to make their move 
       this.turn =
         this.player_1.designatedSymbol() === this.turn
           ? this.player_2.designatedSymbol()
@@ -293,35 +304,30 @@ class Game {
   }
 }
 
-// console.log(player_2.designatedSymbol());
+//global variable for game object
+let gameObject;
 
 //querySelector returns an object object.methodNmae
 document.querySelector("#play-game").addEventListener("click", startGame);
-//trying to loop thru to add an eventlistner to be able to add x's and o's
 
-//added a variable to store my array of elements
+//trying to loop thru to add an eventlistner to be able to add x's and o's added a variable to store my array of elements
 const cellItems = document.querySelectorAll("div.cell-item");
-
 console.log(cellItems);
-
-//global variable to reference where I want is so i can refeernce it in multiple functions bc if i did it inside it other functions it would be a local scope not being able to acess
-
-let gameObject;
 
 //added event listers to all cells
 cellItems.forEach((cellItem) => {
   cellItem.addEventListener("click", clickableCell);
 });
 
-//pass event as parameter to give me the event that happens
+
+//adds event handlers to cell when selected
 function clickableCell(event) {
   gameObject.takeTurn(event.target);
   console.log("cellItems");
   console.log(event.target.id);
 }
 
-// let allXandOs =
-
+//this is an event handler that will trigger a new game 
 function startGame() {
   
   // CREATE AN INSTANCE OF THE GAME OBEJCT
