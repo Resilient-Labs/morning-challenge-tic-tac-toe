f2c04cf5a57e12276b43f69eabf1df3eef3883f7
Michaelariasdls <michaelariasdls@gmail.com>
2023-10-17 22:56:27 -0400

    updated game so it works properly

--- index.js
+++ index.js
@@ -13,10 +13,9 @@
 //the second user needs to have the same ability
 //A click returns a x or O so i need event listeners 
 //I need to have a formula to check if 3 symbols match in the 3x3 grid
+
 //Blocks could be numbered?
 // need to display either a win or loss to the resulting user
-
-
 
 //Ro made a good point of how the game should play as a single turn for each player.
 
@@ -25,73 +24,95 @@
 //clear the game same as refresh
 //anounces winner or loser "Player one/X wins! - Player two/O wins!"
 
-let player = "x";
+//if else conditional to check the winConditions
+let playerOne = "x";
+let playerTwo = "o";
+let currentPlayer = playerOne; 
 let cell = document.querySelectorAll(".cells");
 let restart = document.getElementById("restart");
-const winConditions = [           //array with all possible matching wins combinations cond's
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-];
-let options = ["", "", "", "", "", "", "", "", ""];
-let running = true;
+restart.addEventListener("click", clearButton);
+// const winConditions = [           //array with all possible matching wins combinations cond's
+//     [0, 1, 2],
+//     [3, 4, 5],
+//     [6, 7, 8],          // 8 Possible Win's
+//     [0, 3, 6],
+//     [1, 4, 7],
+//     [2, 5, 8],
+//     [0, 4, 8],
+//     [2, 4, 6],
+// ];
+// let selectCells = ["", "", "", "", "", "", "", "", ""];    // 9 Cell's
+// let running = true;
+// || !running
 
 cell = Array.from(cell);
 
+function clearButton(){
+    cell.forEach(element => {
+        element.innerText = ""
+    })
+  }
+
 cell.forEach(element => {
     element.addEventListener("click", () => {
-        if (element.innerText !== "" || !game.session) {
+        if (element.innerText !== "") {
             return;
         }
-
-        element.innerText = player;
-
-        game.winner();
-        game.checkDraw();
-
-        player = player === "x" ? "o" : "x";
-    });
-});
-
-class Create {
-    constructor() {
-        this.session = true;
-    }
-
-    checkDraw() {
-        if (!this.session) {
-            return;
-        }
-        let draw = cell.every(element => element.innerText == "x" || element.innerText == "o");
-
-        if (draw) {
-            alert('draw');
-            this.session = false;
-        }
-    }
-    
-
-
-
-
-    winner() {
         
-            //Slot machine Win form
-        function checkWin(results) {
-            console.log(results)
-            // console.log(results[0])
-            const firstSymbol = results[0];
-            const isWin = results.every(symbol => symbol === firstSymbol);
-            //every method is like a for( Loop ) goes through every element 
-            return isWin;
+        const boardGame = new Board();
+        element.innerText = currentPlayer;
+        
+        boardGame.checkDraw()
+        boardGame.testWinner()
 
-    // Implement the 'winner' method to check for win conditions
-}
+        // document.createElement()
 
-const game = new Create();
-restart.addEventListener('click', () => game.runAgain());
+        currentPlayer = currentPlayer === playerOne ? playerTwo: playerOne
+      })})
+
+        class Board {
+                checkDraw() {
+                    let finished = cell.every((element, index) => cell[index].innerText == "x" || cell[index].innerText == "o")
+                  
+                    if(finished){
+                      document.querySelector("#statusText").innerText = `It is a draw!` // Insert text to show who won
+                      console.log('testerr')
+                    }
+                  }   
+ 
+
+         testWinner() {   //this applies to the first combo of win conditions
+            console.log('.');
+            //first sol
+            if (cell[0].innerText == currentPlayer && cell[1].innerText == currentPlayer && cell[2].innerText == currentPlayer) {
+                document.querySelector("#statusText").innerText = `${currentPlayer} Wins YAAY!`
+            }
+
+             else if (cell[3].innerText == currentPlayer && cell[4].innerText == currentPlayer && cell[5].innerText == currentPlayer) {
+                document.querySelector("#statusText").innerText = `${currentPlayer} Wins YAAY!`
+             }
+
+             else if (cell[6].innerText == currentPlayer && cell[7].innerText == currentPlayer && cell[8].innerText == currentPlayer) {
+                document.querySelector("#statusText").innerText = `${currentPlayer} Wins YAAY!`
+             }
+             else if (cell[0].innerText == currentPlayer && cell[3].innerText == currentPlayer && cell[6].innerText == currentPlayer) {
+                document.querySelector("#statusText").innerText = `${currentPlayer} Wins YAAY!`      
+             }
+             else if (cell[1].innerText == currentPlayer && cell[4].innerText == currentPlayer && cell[7].innerText == currentPlayer) {
+                document.querySelector("#statusText").innerText = `${currentPlayer} Wins YAAY!`        
+             }
+             else if (cell[2].innerText == currentPlayer && cell[5].innerText == currentPlayer && cell[8].innerText == currentPlayer) {
+                document.querySelector("#statusText").innerText = `${currentPlayer} Wins YAAY!`      
+             }
+             else if (cell[0].innerText == currentPlayer && cell[4].innerText == currentPlayer && cell[8].innerText == currentPlayer) {
+                document.querySelector("#statusText").innerText = `${currentPlayer} Wins YAAY!`   
+             }
+             else if (cell[2].innerText == currentPlayer && cell[4].innerText == currentPlayer && cell[6].innerText == currentPlayer) {
+                document.querySelector("#statusText").innerText = `${currentPlayer} Wins YAAY!`
+                } 
+            }
+        }
+
+        
+    
+    const boardGame = new Board();
